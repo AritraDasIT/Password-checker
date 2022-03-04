6a83053b818569d1d95f8e681c6422692e0b6910
DasAritra2001 <aritradas.stthomasit@gmail.com>
2022-03-04 21:13:36 +0530

    password checker

---
+++ password_checker.c
@@ -0,0 +1,40 @@
+#include<stdio.h>
+int main()
+{
+	char str[10];
+	char str2[10];
+	int i=0;
+	int flag=1;
+	
+	printf("Enter the new  password: ");
+	scanf("%s",str);
+	
+	printf("Enter the  password:");
+	scanf("%s",str2);
+	
+	while(str[i]!='\0')
+	{
+		if(str[i]!=str2[i])
+		{
+			flag=0;
+			
+			break;
+			
+		}
+		i++;
+		
+	}
+	
+	if(flag==1)
+	{
+		printf("You successfully logged in");
+	}
+	else
+	{
+		printf("Failed, please try again");
+	}
+	
+	
+	
+}
+
